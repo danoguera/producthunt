e3cc6af057baabbdb3191ee68d7f27d0eac6c0c1
Diego Noguera <danoguera@gmail.com>
2020-03-09 18:29:34 -0500

    This annotates the models with the DB fields

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string
+#  url         :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ActiveRecord::Base
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string(100)
+#  name            :string(100)
+#  password_digest :string
+#  twitter_handle  :string(50)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
